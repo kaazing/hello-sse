a67778643a35191d8b9afb3c8833b0473c12c650
Mike Dove <mdove@mdove.com>
2019-03-29 12:54:14 -0700

    Added dotnet sample, updated README.md

---
+++ dotnet/ConsoleSSE/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using EvtSource;
+using static System.Net.Http.WinHttpHandler;
+
+namespace ConsoleSSE
+{
+    class Program
+    {
+        //
+        // Use Microsoft's WinHttpHandler (via nuget) to enable HTTP/2
+        //   - extend SendAsync to set HTTP version
+        //   - pass Http2CustomHandler to instantiation of HttpClient()
+        //
+        public class Http2CustomHandler : WinHttpHandler
+        {
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+            {
+                request.Version = new Version("2.0");
+                return base.SendAsync(request, cancellationToken);
+            }
+        }
+        static void Main(string[] args)
+        {
+            string url = "";
+
+            if (args.Length > 0)
+            {
+                url = args[0];
+            }
+            else
+            {
+                Console.WriteLine("Usage: ConsoleSSE <Server-Sent Events URL");
+                System.Environment.Exit(1);
+            }
+
+            var evt = new EventSourceReader(new Uri(url), new Http2CustomHandler()).Start();
+
+            evt.MessageReceived += (object sender, EventSourceMessageEventArgs e) => {
+                Console.WriteLine($"{e.Message}");
+            };
+            evt.Disconnected += async (object sender, DisconnectEventArgs e) => {
+                Console.WriteLine($"Retry: {e.ReconnectDelay} - Error");
+                await Task.Delay(e.ReconnectDelay);
+                evt.Start(); // Reconnect to the same URL
+            };
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
--- dotnet/README.md
+++ dotnet/README.md
@@ -1,3 +1,29 @@
 # Using .NET Core 2.0/.NET Framework 4.7.2 for Server Sent Events
 
-TBD
+Built/Tested with Visual Studio 2019 Preview under Windows 10.
+
+The `App.config` file in repository specifies .NET Framework 4.7.2, but this sample will also compile/run successfully under .NET Core 2.0.
+
+This sample relies on two packages from NuGet which Visual Studio should install automatically:
+- 3v.EvtSource 1.1.1 - Implementation of Server-Sent Events standard
+- WinHttpHandler 4.6.0 - Microsoft package which enables the program to use HTTP/2
+
+## Instructions to Build/Run:
+- Open Visual Studio
+- Under "File/Open" select "Project/Solution"
+- Select `ConsoleSSE.sln`
+- Under "Build" select "Build Solution"
+- If build fails because Visual Studio didn't install `3v.EvtSource` and/or `WinHttpHandler` automatically:
+    - Under "Project" select "Manage NuGet Packages"
+    - Under Browse:
+        - Search for `3v.EvtSource` and install
+        - Search for `WinHttpHandler` and install
+    
+- Run from Visual Studio:
+    - Under "Debug" select "Start Debugging"
+- Run from Command Prompt/Power Shell:
+    - Navigate to ConsoleSSE\ConsoleSSE\bin\Debug directory
+    - Run `.\ConsoleSSE.exe <Server-Sent Events URL>`
+
+
+
