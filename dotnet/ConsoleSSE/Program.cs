3ac779a7c06226ea69335c3a3041b20c5251db5a
Mike Dove <mdove@mdove.com>
2019-05-10 14:30:05 -0700

    Added support for multiple command line arguments, force single connection per server on H2, added comments around reconnects

--- dotnet/ConsoleSSE/Program.cs
+++ dotnet/ConsoleSSE/Program.cs
@@ -27,28 +27,37 @@ namespace ConsoleSSE
         }
         static void Main(string[] args)
         {
-            string url = "";
+            // Create custom handler for HTTP/2
+            var h2handler = new Http2CustomHandler();
 
-            if (args.Length > 0)
+            // Set HTTP/2 to use only 1 connection per server to force connection sharing
+            h2handler.MaxConnectionsPerServer = 1;
+
+            if (args.Length == 0)
             {
-                url = args[0];
-            }
-            else
-            {
-                Console.WriteLine("Usage: ConsoleSSE <Server-Sent Events URL");
+                Console.WriteLine("Usage: ConsoleSSE <Server-Sent Events URL> [<Server-Sent Events URL>] ...");
                 System.Environment.Exit(1);
             }
+            
+            for (int i = 0; i < args.Length; i++)
+            {
+                var evt = new EventSourceReader(new Uri(args[i]), h2handler).Start();
 
-            var evt = new EventSourceReader(new Uri(url), new Http2CustomHandler()).Start();
-
-            evt.MessageReceived += (object sender, EventSourceMessageEventArgs e) => {
-                Console.WriteLine($"{e.Message}");
-            };
-            evt.Disconnected += async (object sender, DisconnectEventArgs e) => {
-                Console.WriteLine($"Retry: {e.ReconnectDelay} - Error");
-                await Task.Delay(e.ReconnectDelay);
-                evt.Start(); // Reconnect to the same URL
-            };
+                evt.MessageReceived += (object sender, EventSourceMessageEventArgs e) => {
+                    Console.WriteLine($"{e.Message}");
+                };
+                evt.Disconnected += async (object sender, DisconnectEventArgs e) =>
+                {
+                    // Default ReceiveDataTimeout is 30 seconds, thus is EventSource data is not received for 30 seconds
+                    // this will be triggered to reconnect.  If you'd like to set a longer timeout, add
+                    // h2handler.ReceiveDataTimeout = new TimeSpan(days, hours, mins, sec) after h2handler is
+                    // instantiated above.  Given reconnects are automatic, the Console.WriteLine message
+                    // isn't required, but left in for informational purposes.
+                    Console.WriteLine($"Receive Timeout: {e.ReconnectDelay}ms - Reconnecting...");
+                    await Task.Delay(e.ReconnectDelay);
+                    evt.Start(); // Reconnect to the same URL
+                };
+            }
 
             while (true)
             {
